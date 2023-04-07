4d72dea0ef1b6664e8808ccf43fdb979ad4fac61
Ninja <cjain@deqode.com>
2023-04-07 11:20:39 +0530

    feat(add tasks): JS function for adding task to the todo list
    Todo list tasks added by creating elements using javascript and validating all the inputs

---
+++ todo.js
@@ -0,0 +1,15 @@
+function addHandler() {
+    var li = document.createElement("li");
+    var inputVal = document.getElementById("input1").value;
+    document.getElementById("input1").value="";
+    var t = document.createTextNode(inputVal);
+    li.classList.add("todoNodes");
+    li.appendChild(t);
+    if (inputVal.trim() == "" || inputVal == null) {
+      alert("Please enter a value");
+      return;
+    } else {
+      document.getElementById("myUl").appendChild(li);
+      li.addEventListener("click", removeTask);
+    }
+  }
